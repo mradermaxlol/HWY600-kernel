77696dfee498cb4f4b94f7f28838d46582b49922
Maxim Sheviakov <mrader3940@openmailbox.org>
2016-01-13 23:34:17 +0300

    Add a helper script This one lets you ease the process of image-making

---
+++ bootimager.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+###
+### Bootimager by mradermaxlol
+###
+### Pretty damn primitive, but effective!
+###
+### How to use it?
+### So, you put a kernel binary into ~/ dir, aka home directory and you name
+### it as kernel.bin
+### Then, you cd your ramdisk dir and run this script, like
+### [~/ramdisk]: $ sh ~/bootimager.sh
+### And it packs your ramdisk, then uses it and the kernel binary to build
+### a working boot.img with mkbootimg
+###
+echo 'Creating a compressed cpio archive...'
+find . | cpio -o -h newc | gzip > ramdisk.cpio.gz
+echo ''
+echo 'Creating boot.img...'
+mkbootimg --kernel ~/kernel.bin --ramdisk ramdisk.cpio.gz -o ~/boot.img
+echo ''
+echo 'Done!'
